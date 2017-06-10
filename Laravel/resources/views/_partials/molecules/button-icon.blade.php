1183a51504ea6f82fce5bae003298c5bb64ec3cc
Fabian <pecherfabian@gmail.com>
2017-06-10 13:04:32 +0200

    removed laravel

--- Laravel/resources/views/_partials/molecules/button-icon.blade.php
+++
@@ -1,3 +0,0 @@
-<button type="button" class="button-icon button-icon--{{ $buttonIconBackgroundcolor }} " >
-        <span class="button-icon__icon icon icon--{{ $buttonIconIcon }}"></span>
-</button>
--- Laravel/resources/views/_partials/molecules/hero-image.blade.php
+++
@@ -1,2 +0,0 @@
-<div class="hero-image" style="background-image: url('/uploads/beachcourts/{{ $id }}/heroimage/{{ $heroImage }}')" >
-<div class="hero-image__overlay"></div>
